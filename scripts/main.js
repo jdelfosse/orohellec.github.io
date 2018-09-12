e0f5715882e9a8030ae716b5c6e8ee49c9014b4e
Olivier Rohellec <olivier@MacBook-Air-de-Olivier.local>
2018-09-12 16:00:59 +0200

    main.js modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,4 @@
-alert("Hey, ce site est trop bien mais ça manque de filles non? Viens par ici pour t'amuser un peu");
+<script>
+	alert("Hey, ce site est trop bien mais ça manque de filles non? Viens par ici pour t'amuser un peu");
+
+</script>
