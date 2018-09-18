25c934185867db07bff4fe438cb033afa340028c
Olivier Rohellec <olivier@macbook-air-de-olivier.home>
2018-09-18 16:50:31 +0200

    le minimum est fait

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,29 @@
-<script>
-	alert("Hey, ce site est trop bien mais ça manque de filles non? Viens par ici pour t'amuser un peu");
 
-	setTimeout("window.close();", 3000);
+setTimeout(function() 
+{
+	if (confirm("Hey, ce site est trop bien mais ça manque de filles non? Viens rejoindre les meufs les plus chaudes de ta région sur xxxvidsxxx"))
+	{
+		document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	};
+}, 3000);
 
-</script>
+
+// document.body.style.backgroundColor = "white";
+
+do {
+	document.getElementById("wtf").onclick = function()
+	{
+		let i = 1;
+		document.body.style.backgroundColor = "purple";
+		document.getElementById("wtf").innerHTML = "go back to normal";
+	}
+		document.getElementById("wtf").onclick = function()
+		{
+			document.body.style.backgroundColor = "white";
+			document.getElementById("wtf").innerHTML = "take some drugs, then, click me again. You'll enjoy it better Mother Fucker";
+			i = 0;
+		}
+} while(i === 0);
+
+
+//	document.getElementById("container").style.backgroundColor = "purple";
